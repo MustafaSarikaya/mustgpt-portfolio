cabeb3cd0076693b3bca8b7fa73a5cc0d3dcb3a5
MustafaSarikaya <msarikay@uottawa.ca>
2025-03-27 17:56:53 -0400

    fix the testimonial flow

--- src/pages/testimonials/TestimonialTwo.tsx
+++ src/pages/testimonials/TestimonialTwo.tsx
@@ -4,24 +4,22 @@ import { getText } from '../../utils/dictionary';
 
 const TestimonialTwo = () => {
   return (
-    <div className="bg-gradient-to-b from-primary to-white">
-      <ChatFlow title={getText('pages.testimonials.two.title')}>
-        <div className="space-y-6">
-          <p className="text-lg text-gray-700">
-            {getText('pages.testimonials.two.mainQuote')}
+    <ChatFlow title={getText('pages.testimonials.two.title')}>
+      <div >
+        <p className="text-lg text-gray-700">
+          {getText('pages.testimonials.two.mainQuote')}
+        </p>
+        <div className="border-l-4 border-accent pl-4">
+          <p className="text-gray-600 italic">
+            {getText('pages.testimonials.two.highlightedQuote')}
           </p>
-          <div className="border-l-4 border-accent pl-4">
-            <p className="text-gray-600 italic">
-              {getText('pages.testimonials.two.highlightedQuote')}
-            </p>
-          </div>
-          <div className="mt-8">
-            <p className="font-semibold">{getText('pages.testimonials.two.author.name')}</p>
-            <p className="text-gray-500">{getText('pages.testimonials.two.author.title')}</p>
-          </div>
         </div>
-      </ChatFlow>
-    </div>
+        <div className="mt-8">
+          <p className="font-semibold">{getText('pages.testimonials.two.author.name')}</p>
+          <p className="text-gray-500">{getText('pages.testimonials.two.author.title')}</p>
+        </div>
+      </div>
+    </ChatFlow>
   );
 };
 
